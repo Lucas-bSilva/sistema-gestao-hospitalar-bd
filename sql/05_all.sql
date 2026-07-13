a1d67e09115954a4a6aa315ba9e5fe02be6a8053
Lucas Barros <lucasbarroscc@gmail.com>
2026-07-13 15:45:27 -0300

    Evita alteracoes permanentes no script de preparacao do banco

--- sql/05_all.sql
+++ sql/05_all.sql
@@ -1,8 +1,12 @@
--- Execucao completa pelo psql.
--- No pgAdmin, recomenda-se executar os arquivos separadamente.
+/*
+
+Este arquivo deve ser executado pelo cliente psql.
+Ele recria a estrutura, insere a carga inicial e valida os requisitos mínimos.
+
+As demonstracoes de CRUD e consultas devem ser executadas separadamente
+no pgAdmin, por meio dos arquivos 03, 04 e 07.
+*/
 
 \i sql/01_schema.sql
 \i sql/02_seed.sql
 \i sql/06_validacoes.sql
-\i sql/03_crud_consultas.sql
-\i sql/04_consultas_analiticas.sql
