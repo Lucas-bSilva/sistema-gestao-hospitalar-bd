313cbe1805e24205456a7d9d0fa12a50288e4502
Lucas Barros <lucasbarroscc@gmail.com>
2026-07-26 17:41:23 -0300

    Automatiza execução completa da Etapa 2

--- sql/etapa2/14_all_etapa2.sql
+++ sql/etapa2/14_all_etapa2.sql
@@ -1,8 +1,12 @@
 /*
 Execução completa pelo cliente psql.
+
 Executar a partir da raiz do repositório:
 
-psql -U postgres -d hospital_yuska -f sql/etapa2/14_all_etapa2.sql
+psql -U postgres -d hospital_yusca -f sql/etapa2/14_all_etapa2.sql
+
+Os comandos \ir são específicos do cliente psql e não funcionam diretamente
+no Query Tool do pgAdmin.
 */
 
 \set ON_ERROR_STOP on
@@ -10,6 +14,7 @@ psql -U postgres -d hospital_yuska -f sql/etapa2/14_all_etapa2.sql
 \ir ../01_estrutura.sql
 \ir ../02_dados_teste.sql
 \ir ../06_validacoes.sql
+
 \ir 08_evolucao_estrutura.sql
 \ir 09_dados_complementares.sql
 \ir 10_procedures.sql
